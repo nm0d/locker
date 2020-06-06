46ccf073e726917f06f3f6dcfedd020f4188d25d
demen <demen.gueler@uni-tuebingen.de>
2020-06-07 00:10:34 +0200

    test if crow is connected and bug fixes

--- lib/ui.lua
+++ lib/ui.lua
@@ -1,28 +1,41 @@
+
 -- ui stuff
 
 local gate_length_multipliers_text = {"1/32","1/16","1/8","1/4","1/2","1","2","3","4","5","6","7","8","16","32","64"}
 
+
+
 function ui_init()
-   screen.aa(1)
+   screen.aa(0)
 end
 
 
 function ui_lock(x, y)
-   screen.level(15)
+   screen.level(4)
    screen.rect(x,  y, 12, 12)
    screen.fill()
 
+   screen.aa(1)
    screen.level(0)
-   screen.circle(x+6, y+4, 3)
+   screen.circle(x+6, y+4, 2)
+   screen.fill()
+   screen.aa(0)
+   
+  
+   
+   screen.level(1)
+   screen.rect(x+4, y+7,4,1)
    screen.fill()
    
    screen.level(0)
-   screen.rect(x+4,  y+4,4 , 6)
+   screen.rect(x+5,  y+4,2 , 4)
    screen.fill()
 
    screen.level(15)
+   screen.line_width(2)
    screen.arc(x+6, y-3,4, math.pi , 0)
    screen.stroke()
+
    
    screen.level(15)
    screen.move(x+10, y-3)
@@ -31,10 +44,12 @@ function ui_lock(x, y)
    screen.move(x+2, y-3)
    screen.line(x+2, y)
    screen.stroke()
+
+   screen.line_width(1)
 end
 
 
-function ui_keys(x,y,n)
+function ui_keys_graphic(x,y,n)
    screen.level(15)
    local key_positions = {}
    
@@ -44,7 +59,7 @@ function ui_keys(x,y,n)
    key_positions[4] = 6
    key_positions[5] = 8
    key_positions[6] = 12
-   key_positions[7] = 15
+   key_positions[7] = 14
    key_positions[8] = 16
    key_positions[9] = 18
    key_positions[10] =20
@@ -101,16 +116,19 @@ function ui_keys(x,y,n)
    screen.fill()
    
    if black_keys[n] then
-      for i = 1,9 do
-	 screen.level(i)
-	 screen.rect(x+ key_positions[n],  y+i, 3, 1)
-	 screen.fill()
-      end
+      screen.level(2)
+      screen.rect(x+ key_positions[n],  y, 3, 10)
+      screen.fill()
+     -- for i = 1,9 do
+
+     --	 screen.rect(x+ key_positions[n],  y+i, 3, 1)
+     --	 screen.fill()
+     -- end
    end
    
 end
 
-function ui_gate(x,y,n)
+function ui_gate_graphic(x,y,j)
    local dark = 1
    local middle = 3
    local bright = 4
@@ -150,8 +168,9 @@ function ui_gate(x,y,n)
 
    local bar_vert = 15
    local bar_hor = 13
-   
-   if n ==0 then
+
+   crow.output[j].query()
+   if crow_out_voltage_hist[j][1] < 0.05 then
       screen.level(bar_hor)
       screen.rect(x+2, y+6, 11,1)
       screen.rect(x+2, y+11, 11,1)
@@ -179,6 +198,31 @@ function ui_gate(x,y,n)
    end
 end
 
+
+function ui_length_info(x,y,j)
+   screen.level(15)
+   screen.move(x, y)
+   screen.text(data[j].length)
+   screen.move(x+10, y)
+   screen.text("steps")
+   
+   screen.move(x+58, y)
+   screen.text_right(data[j].mult)
+   
+   screen.move(x+60, y)
+   screen.text("x")
+
+   if data[j].mute == 1 then 
+      screen.move(x+67, y)
+      screen.text("mute")
+      
+   end
+
+   
+end
+
+
+
 function lightning(x,y) -- not used, but pretty
    local dark =4
    
@@ -195,26 +239,25 @@ function lightning(x,y) -- not used, but pretty
    screen.fill()
 end
 
-function ui_cv(x,y,data, j)
+function ui_cv_graphic(x,y, j)
    crow.output[j].query()
 
-   screen.level(13)
+   screen.level(15)
    
-   --screen.move(x,y -math.floor(crow_out_voltages[j]/5 * 7)+7)
-   --screen.line(x+30,y -math.floor(crow_out_voltages[j] /5 *7)+7)
-   for i =1,queue_length do
-      screen.pixel(x+queue_length -i,y -math.floor(crow_out_voltage_hist[j][i]/5 * 7)+7)
+   for i =2,queue_length do
+      screen.move(x +queue_length -i, y -math.floor(crow_out_voltage_hist[j][i]/5 * 7)+7)
+      screen.line(x +queue_length - (i+1), y -math.floor(crow_out_voltage_hist[j][i-1]/5 * 7)+7)
+      
+      if i % 3 == 0 then
+	 
+      end
    end
-   
-   
-   screen.fill()
+   --  screen.fill()
+   screen.stroke()
 end
 
 
-
-
-
-function ui_gate_lock(data, i,j)
+function ui_gate_lock(i,j)
    ui_lock(0,9)
    
    screen.level(15)
@@ -235,16 +278,20 @@ function ui_gate_lock(data, i,j)
    screen.fill()
 end
 
-function ui_cv_lock(data, i, j)
+
+function ui_cv_lock(i, j)
    ui_lock(0,9)
    screen.level(15)
 
    screen.move(15, 10)
-   screen.text("control")
-   screen.move(15, 20)
-   screen.text("voltage")
-   
-   screen.move(55, 10)
+   screen.text("control voltage ")
+
+
+
+   screen.move(15, 30)
+   screen.text("slew time")
+
+   screen.move(80, 10)
 
    if data[j].cv[i] == 16 then
       screen.text(0)
@@ -252,16 +299,54 @@ function ui_cv_lock(data, i, j)
       screen.text((math.floor((16- data[j].cv[i]) * -5/15 *1000) )/1000  )
    else
       screen.text(math.floor( (data[j].cv[i]-16) * 5/15 *1000)/1000 ) 
-
    end
-
-
-   screen.move(55, 20)
-
+   screen.move(106, 10)
    screen.text("volts")
+
+
+   screen.move(80, 30)
+--   screen.text(math.floor(((data[j].slew_time[i]-1)/params:get("step_div") *1000))/1000)
+   screen.text(data[j].slew_time[i]-1)
+
+
+   screen.move(106, 30)
+   screen.text("steps")
+   
    screen.fill()
+
+
+
 end
 
 
-
+function ui_highlight_mode()
+   screen.level(1)
+   if grid_state == 2 then
+      screen.rect(40,1,34,64)
+      screen.fill()
+   elseif grid_state == 3 then
+      screen.rect(91,1,13,64)
+      screen.fill()
+   elseif grid_state == 4 then
+      screen.rect(106,1,21,64)
+      screen.fill()
+   elseif grid_state == 5 then
+      screen.rect(1,1,30,64)
+      screen.fill()     
+   end
+   
+   
+end
+function ui_crow_disconnected()
+   if math.floor(clock.get_beats()) %4  == 1 then
+         screen.display_png("home/we/dust/code/dmon/locker/crow_disconnected_2.png", 0, 0)
+   else
+      screen.display_png("home/we/dust/code/dmon/locker/crow_disconnected_1.png", 0, 0)
+   end
+   screen.level(13)
+   screen.move(0,20)
+   screen.text("crow")
+   screen.move(0,30)
+   screen.text("disconnected")
+end
    
--- locker.lua
+++ locker.lua
@@ -1,19 +1,17 @@
+
 g = grid.connect()
 
 
 
 ----begin UI stuff
 local ui =  include('lib/ui')
-
-
-
 --- end UI stuff
 
 
 data = {}
-for i = 1,4 do
-   data[i] = {
-      pos = 0,
+for j = 1,4 do
+   data[j] = {
+      pos = 1,
       length =16,
       gate =        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
       gate_length = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
@@ -22,10 +20,21 @@ for i = 1,4 do
       cv_bipolar = true,
       note_numbers = {13,13,13,13,13,13,13,13,13,13,13,13,13,13,13,13},
       octave = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-      slew_time=  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-      
+      slew_time=  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+      mult = 1,
+      mult_pos = 1,
+      mute = 0
    }
 end
+
+grid_state = 0 -- nothing pressed
+-- 1 trig held
+-- 2 set lengths
+-- 3 set mults
+-- 4 set mutes
+-- 5 set seq_type
+
+play_state = 0 -- not playing
 
 
 
@@ -124,6 +133,21 @@ for x = 1,16 do
    end
 end
 
+
+function trig_held()
+   for x = 1,16 do
+      for y = 1,4 do
+	 if pressed[x][y] then
+	    return true
+	 end
+      end
+   end
+   return false
+end
+
+
+
+
    
 function neighbor_index(row_index) 
    for k =1,15 do
@@ -156,43 +180,49 @@ function step()
    while true do
       clock.sync(1/params:get("step_div"))
       for j=1,4 do
-	 data[j].pos = (data[j].pos % data[j].length) + 1
-	 if data[j].seq_type == 1 then -- gate sequence
-	    if data[j].gate[data[j].pos] > 0 then
-	       crow.output[j].action = "{ to(5,0), to(0," .. gate_length_multipliers[data[j].gate_length[data[j].pos]] * (clock.get_beat_sec()/(params:get("step_div"))) .. ") }"
-	       crow.output[j]()	 
-	    end
-	 elseif data[j].seq_type == 2 then --cv sequence
-	    
-	    if data[j].gate[data[j].pos] > 0 then
-	       crow.output[j].slew = (data[j].slew_time[data[j].pos] -1) * (clock.get_beat_sec()/(params:get("step_div")))
-	       if data[j].cv[data[j].pos] == 16 then
-		  crow.output[j].volts = 0
-	       elseif data[j].cv[data[j].pos] < 16 then
-		  crow.output[j].volts = (16 - data[j].cv[data[j].pos]) * -5/15
-	       else
-		  crow.output[j].volts = (data[j].cv[data[j].pos]-16) * 5/15
+	 if data[j].mult_pos % data[j].mult == 0 then
+	    if data[j].mute == 0 then
+	       if data[j].seq_type == 1 then -- gate sequence
+		  if data[j].gate[data[j].pos] > 0 then
+		     crow.output[j].action = "{ to(5,0), to(0," .. gate_length_multipliers[data[j].gate_length[data[j].pos]] * (clock.get_beat_sec()/(params:get("step_div"))) .. ") }"
+		     crow.output[j]()	 
+		  end
+	       elseif data[j].seq_type == 2 then --cv sequence
+		  if data[j].gate[data[j].pos] > 0 then
+		     crow.output[j].slew = (data[j].slew_time[data[j].pos] -1) * (clock.get_beat_sec()/(params:get("step_div")))
+		     if data[j].cv[data[j].pos] == 16 then
+			crow.output[j].volts = 0
+		     elseif data[j].cv[data[j].pos] < 16 then
+			crow.output[j].volts = (16 - data[j].cv[data[j].pos]) * -5/15
+		     else
+			crow.output[j].volts = (data[j].cv[data[j].pos]-16) * 5/15
+		     end
+		  end
+	       elseif data[j].seq_type == 3 then -- v/8 sequence
+		  if data[j].gate[data[j].pos] > 0 then
+		     crow.output[j].volts =  (data[j].note_numbers[data[j].pos] + (data[j].octave[data[j].pos] * 12) )/12
+		  end
 	       end
-	    
 	    end
-	    
-	 elseif data[j].seq_type == 3 then -- v/8 sequence
-	    
-	    if data[j].gate[data[j].pos] > 0 then
-	       crow.output[j].volts =  (data[j].note_numbers[data[j].pos] + (data[j].octave[data[j].pos] * 12) )/12
-	       
-	    end
-	    
+	    data[j].pos = (data[j].pos % data[j].length) + 1
 	 end
+	 data[j].mult_pos = (data[j].mult_pos % data[j].mult) + 1
       end
-      
-      
       if g then
 	 gridredraw()
       end
       redraw()      
    end
 end
+
+function resync()
+   for j = 1,4 do
+      data[j].pos = 1
+      data[j].mult_pos = 1      
+   end
+end
+
+
 
 function init()
    params:add{type = "number", id = "step_div", name = "step division", default = 4}
@@ -211,13 +241,26 @@ function init()
    --- end UI stuff
    
    for j = 1,4 do
-      params:add{type = "number", id = j.. "_length", name = j .." length", min =1, max = 16, default = data[j].length,
-		 action=function(x) data[j].length = x end }
-
       params:add{type = "option", id= j .."_seq_type", name= j .." seq type",
-	      options= seq_type_names, default = 1,
-	      action = function(x) data[j].seq_type = x end }
-         params:add_separator()
+		 options= seq_type_names, default = 1,
+		 action = function(x) data[j].seq_type = x end }
+
+      params:add{type = "number",
+		 id = j.. "_length",
+		 name = j .." length",
+		 min =1, max = 16,
+		 default = data[j].length,
+		 action=function(x) data[j].length = x end }
+      
+      params:add{type = "number",
+		 id = j.. "_multiplier",
+		 name = j .." multiplier",
+		 min =1, max = 16,
+		 default = data[j].mult,
+		 action=function(x) data[j].mult = x end }
+      
+      
+      params:add_separator()
    end
    
  
@@ -237,33 +280,48 @@ end
 
 function redraw()
    screen.clear()
-   for j =1,4 do
-      for i =1,16 do
-	 if pressed[i][j] then
-	    if data[j].seq_type == 1 then
-	       ui_gate_lock(data, i,j)
-	    elseif data[j].seq_type == 2 then
-	       ui_cv_lock(data, i, j)
-	    elseif data[j].seq_type == 3 then
+   local crow_connected = crow.connected()
+
+   if not crow_connected then
+      ui_crow_disconnected()
+   else
+      if grid_state == 1 then
+	 for j =1,4 do
+	    for i =1,16 do
+	       if pressed[i][j] then
+		  if data[j].seq_type == 1 then
+		     ui_gate_lock(i,j)
+		  elseif data[j].seq_type == 2 then
+		     ui_cv_lock(i, j)
+		  elseif data[j].seq_type == 3 then
+		  end
+		  break
+	       end
 	    end
-	    break
 	 end
       end
-   end
-   -- TODO: if nothing is pressed then
-   -- begin
-   for j =1,4 do
-      if data[j].seq_type == 1 then
-	 ui_gate(1 ,(j-1) *16 +1, data[j].gate[data[j].pos])
-      elseif data[j].seq_type == 2 then
-	 ui_cv(1 ,(j-1) *16 +1, data,j)
-      elseif data[j].seq_type == 3 then
-	 if data[j].gate[data[j].pos] == 0 then
-	    ui_keys(1 ,(j-1) *16 +1, 0)
-	 else
-	    ui_keys(1 ,(j-1) *16 +1, math.random(1,12))
-	 end	 
+      
+      if grid_state ~= 1 then
+	 ui_highlight_mode()	 
+	 
+	 for j =1,4 do
+	    ui_length_info(40,(j-1) *16 +9, j)
+	    if data[j].seq_type == 1 then
+	       ui_gate_graphic(1 ,(j-1) *16 +1, j)
+	    elseif data[j].seq_type == 2 then
+	       ui_cv_graphic(1 ,(j-1) *16 +1,j)
+	    elseif data[j].seq_type == 3 then
+	       if data[j].gate[data[j].pos] == 0 then
+		  ui_keys_graphic(1 ,(j-1) *16 +1, 0)
+	       else
+		  ui_keys_graphic(1,
+				  (j-1) *16 +1,
+				  ((data[j].note_numbers[data[j].pos]-1) % 12)+1) 
+	       end	 
+	    end
+	 end
       end
+      
    end
    --end
    screen.update()   
@@ -272,20 +330,20 @@ end
 function gridredraw()
    g:all(0)
 
-   
-   for j= 1,4 do
-      for i = 1,16 do
-	 if data[j].gate[i] > 0 then
-	    g:led(i,j,10)
+   if grid_state == 0 or grid_state == 1 then
+      for j= 1,4 do
+	 for i = 1,16 do
+	    if data[j].gate[i] > 0 then
+	       g:led(i,j,10)
+	    end
 	 end
       end
    end
    
    
-   
    for j = 1,4 do
       for i = 1,16 do
-	 if pressed[i][j] then
+	 if pressed[i][j] and (grid_state == 0 or grid_state == 1) then
 	    if data[j].seq_type == 1 then -- gate
 	       for k =1,16 do
 		  g:led(k,5,3)
@@ -335,13 +393,65 @@ function gridredraw()
 		  g:led(15,6,2 + 8 * (math.floor(clock.get_beats()) %2))
 		  g:led(16,6,2) 
 	       end
-	       
 	    end
-	       
+	 end
+      end
+      
+      if grid_state == 0 then
+	 g:led(1,5,5)
+	 g:led(2,5,5)
+	 g:led(3,5,5)
+	 g:led(4,5,5)
+	 
+	 if pressed[16][5] then
+	    g:led(16,5,13)
+	 else
+	    g:led(16,5,5)
 	 end
 	 
       end
-      g:led(data[j].pos, j, 4) 
+      
+	 
+      if grid_state == 2 then -- length state
+	 g:led(2,5,13)
+	 for j = 1,4 do
+	    for i = 1,data[j].length do
+	       g:led(i,j,3)
+	    end
+	    g:led(data[j].length,j,10)
+	 end
+	 
+      elseif grid_state == 3 then -- mult state
+	 g:led(3,5,13)
+	 for j = 1,4 do
+	    for i = 1,16 do
+	       if i > data[j].mult then g:led(i,j,3) end
+	    end
+	    g:led(data[j].mult,j,10)
+	 end
+      elseif grid_state == 4 then --  mute state
+	 g:led(4,5,13)
+	 for j = 1,4 do
+	    for i = 1,16 do
+	       if data[j].mute == 0 then g:led(i,j,3) end
+	    end
+	 end
+      elseif grid_state == 5 then --seq_type state
+	 g:led(1,5,13)
+	 for j = 1,4 do
+	    for i = 1,3 do
+	        g:led(i,j,3)
+	    end
+	    g:led(data[j].seq_type,j,10)
+	 end
+      end
+      if data[j].mute == 0 then
+	 g:led(data[j].pos, j, 4)
+      end
+
+      
+      
+      
    end
    
    
@@ -392,10 +502,10 @@ function g.key(x,y,z)
    end
 
    
-   if y <=4 then
+   if y <=4 and (grid_state == 0 or grid_state == 1) then
       if z == 1 then
 	 down_time = util.time() 
-
+	 grid_state = 1 -- set locking state
 	 
 	 if data[y].gate[x] == 0 then
 	    data[y].gate[x] =1
@@ -403,6 +513,7 @@ function g.key(x,y,z)
 	 end
       else
 	 hold_time = util.time() - down_time
+	 grid_state = 0 -- set nothing pressed state
 	 
 	 if hold_time < 0.3 and data[y].gate[x] == 1 and not set_down  then
 	    data[y].gate[x] =0
@@ -416,7 +527,7 @@ function g.key(x,y,z)
       
       
       
-   elseif y == 5 then
+   elseif y == 5 and grid_state == 1 then
       for j=1,4 do
 	 for i = 1,16 do
 	    if pressed[i][j] then
@@ -439,7 +550,7 @@ function g.key(x,y,z)
 	 end
       end
 
-   elseif y == 6 then 
+   elseif y == 6 and grid_state == 1 then 
             for j=1,4 do
 	 for i = 1,16 do
 	    if pressed[i][j] then
@@ -457,7 +568,7 @@ function g.key(x,y,z)
 	    end
 	 end
       end
-   elseif keyboard_index ~= 0 then -- keyboard locks
+   elseif keyboard_index ~= 0  and grid_state ==1 then -- keyboard locks
        for j=1,4 do
 	  for i = 1,16 do
 	     if pressed[i][j] then
@@ -465,9 +576,58 @@ function g.key(x,y,z)
 	     end
 	  end
        end
-       
-      
    end
+
+   
+   --- begin set track lengths/multipliers/mutes
+   if y == 5 and (grid_state == 0 or grid_state == 2 or grid_state == 3 or grid_state == 4 or grid_state == 5) then
+      if z == 1 then
+	 if x == 1 then
+	    grid_state =5 -- set seq_type state
+	 elseif x == 2 then
+	    grid_state =2 -- set lengths state
+	 elseif x == 3 then
+	    grid_state =3 -- set mult state
+	 elseif x == 4 then
+	    grid_state =4 -- set mute state
+	 end
+      else
+	 grid_state =0 -- set normal state
+      end
+   end
+   
+   if y <= 4 and grid_state == 2 then
+      if z == 1 then
+	 data[y].length = x
+      end
+   end
+
+   if y <= 4 and grid_state == 3 then
+      if z == 1 then
+	 data[y].mult = x
+      end
+   end
+   if y <= 4 and grid_state == 4 then
+      if z == 1 then
+	 data[y].mute = 1 - data[y].mute
+      end
+   end
+   if y <= 4 and x <= 3 and grid_state == 5 then
+      if z == 1 then
+	 data[y].seq_type = x
+      end
+   end
+
+   --- end set track lengths/multipliers/mutes
+
+
+   --- begin transport
+   if y == 5 and x == 16 and z == 1 and grid_state == 0 then
+      resync()
+   end   
+   -- end transport
+
+   
    gridredraw()
    redraw()
 end
